377f4aa174b3f58d122c176b5b0e537f7c9da29d
TatianaMityagina <394999325@mail.ru>
2019-07-15 19:48:16 +0300

    Версия для предзащиты

--- js/preview.js
+++ js/preview.js
@@ -6,52 +6,52 @@
   var ESC_KEYCODE = 27;
 
   var cancelBigPicture = document.querySelector('.big-picture__cancel');
-  var bigPicture = document.querySelector('.big-picture');
+  var bigPictureElement = document.querySelector('.big-picture');
   var body = document.querySelector('body');
-  var commentCount = document.querySelector('.social__comment-count');
-  var commentsLoader = document.querySelector('.comments-loader');
+  var commentCountElement = document.querySelector('.social__comment-count');
+  var commentsLoaderElement = document.querySelector('.comments-loader');
   var createCommentsBlock = window.createCommentsBlock;
   var hideCommentsInfo = window.hideCommentsInfo;
 
-  var onBigPictureEscPress = function (e) {
-    if (e.keyCode === ESC_KEYCODE) {
+  var onBigPictureEscPress = function (evt) {
+    if (evt.keyCode === ESC_KEYCODE) {
       closeBigPicture();
     }
   };
 
   window.showBigPicture = function (pictureData) {
-    bigPicture.querySelector('.big-picture__img img').src = pictureData.url;
-    bigPicture.querySelector('.likes-count').textContent = pictureData.likes;
-    bigPicture.querySelector('.comments-count').textContent = pictureData.comments.length;
-    bigPicture.querySelector('.social__caption').textContent = pictureData.description;
+    bigPictureElement.querySelector('.big-picture__img img').src = pictureData.url;
+    bigPictureElement.querySelector('.likes-count').textContent = pictureData.likes;
+    bigPictureElement.querySelector('.comments-count').textContent = pictureData.comments.length;
+    bigPictureElement.querySelector('.social__caption').textContent = pictureData.description;
     createCommentsBlock(pictureData.comments);
     if (pictureData.comments.length < 5) {
       hideCommentsInfo();
     } else {
-      commentCount.childNodes[0].data = '5 из ';
+      commentCountElement.childNodes[0].data = '5 из ';
     }
 
     body.classList = 'modal-open';
-    bigPicture.classList.remove('hidden');
+    bigPictureElement.classList.remove('hidden');
     document.addEventListener('keydown', onBigPictureEscPress);
   };
 
   var closeBigPicture = function () {
-    bigPicture.classList.add('hidden');
+    bigPictureElement.classList.add('hidden');
     document.removeEventListener('keydown', onBigPictureEscPress);
     document.querySelector('.img-upload__input').value = '';
     body.classList = '';
     document.removeEventListener('keydown', onBigPictureEscPress);
-    commentsLoader.classList.remove('visually-hidden');
-    commentCount.classList.remove('visually-hidden');
+    commentsLoaderElement.classList.remove('visually-hidden');
+    commentCountElement.classList.remove('visually-hidden');
   };
 
   cancelBigPicture.addEventListener('click', function () {
     closeBigPicture();
   });
 
-  cancelBigPicture.addEventListener('keydown', function (e) {
-    if (e.keyCode === ENTER_KEYCODE) {
+  cancelBigPicture.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === ENTER_KEYCODE) {
       closeBigPicture();
     }
   });
