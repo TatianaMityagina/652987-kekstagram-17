97e0a8c9c4c413044d6b8c13d0fe5fc1a8861dd9
TatianaMityagina <394999325@mail.ru>
2019-07-24 10:17:54 +0300

    Финальная проверка Попытка 2

--- js/backend.js
+++ js/backend.js
@@ -3,44 +3,28 @@
 window.backend = (function () {
   var URL_GET = 'https://js.dump.academy/kekstagram/data';
   var URL_POST = 'https://js.dump.academy/kekstagram';
+  var TIMEOUT = 10000; // ms
+  var STATUS_CODE = 200;
 
   var load = function (onSuccess, onError) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
 
     xhr.addEventListener('load', function () {
-      switch (xhr.status) {
-        case 200:
-          onSuccess(xhr.response);
-          break;
-
-        case 400:
-          onError('Не верный запрос');
-          break;
-        case 401:
-          onError('Пользователь не авторизован');
-          break;
-        case 404:
-          onError('Данных не найдено');
-          break;
-
-        case 500:
-          onError('Ошибка сервера');
-          break;
-
-        default:
-          onError('Cтатус ответа: : ' + xhr.status + ' ' + xhr.statusText
-          );
+      if (xhr.status === STATUS_CODE) {
+        onSuccess(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
       }
     });
-
     xhr.addEventListener('error', function () {
       onError('Произошла ошибка соединения');
     });
     xhr.addEventListener('timeout', function () {
       onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
     });
-    xhr.timeout = 10000;
+
+    xhr.timeout = TIMEOUT;
 
     xhr.open('GET', URL_GET);
     xhr.send();
@@ -51,7 +35,7 @@ window.backend = (function () {
     xhr.responseType = 'json';
 
     xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
+      if (xhr.status === STATUS_CODE) {
         onSuccess(xhr.response);
       } else {
         onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
